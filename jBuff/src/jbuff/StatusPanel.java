95b9d6b2f9ce02db389f1b39e10cb61cf0e127dd
Adam Trojnar <trojnaradam@gmail.com>
2014-01-19 23:28:58 +0100

    first commit of program, not properly worked yet.. still study

---
+++ jBuff/src/jbuff/StatusPanel.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jbuff;
+
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * @author Adam Trojnar
+ * @date Jan 18, 2014
+ */
+public class StatusPanel extends JPanel {
+	private JLabel message;
+	private JLabel dropsTotal;
+	private JLabel dropsCrossing;
+	private JLabel dropsMax;
+	private JLabel needleSize;
+	private JLabel precision;
+	private JLabel estimation;
+
+
+	StatusPanel() {
+		setLayout(new GridLayout(3,1));
+
+		JPanel msgBar = new JPanel(new GridLayout(1,2));
+		msgBar.add(new JLabel("Status:"));
+		msgBar.add(message = new JLabel());
+		message.setHorizontalAlignment(JLabel.LEFT);
+		
+		JPanel infoBar = new JPanel(new GridLayout(1, 4));
+		infoBar.add(new JLabel("Needle Size:"));
+		infoBar.add(needleSize = new JLabel());
+		needleSize.setHorizontalAlignment(JLabel.LEFT);
+
+		infoBar.add(new JLabel("Precision:"));
+		infoBar.add(precision = new JLabel());
+		precision.setHorizontalAlignment(JLabel.LEFT);
+
+
+		JPanel statsBar = new JPanel(new GridLayout(2,5));
+
+		statsBar.add(new JLabel("Max Drops:"));
+		statsBar.add(new JLabel("Dropped:"));
+		statsBar.add(new JLabel("Crossing:"));
+		statsBar.add(new JLabel("Estimated PI:"));
+		statsBar.add(dropsMax = new JLabel());
+		statsBar.add(dropsTotal = new JLabel());
+		statsBar.add(dropsCrossing = new JLabel());
+		statsBar.add(estimation = new JLabel());
+
+		add(msgBar);
+		add(infoBar);
+		add(statsBar);
+
+		resetStatus();
+	}
+
+	public void resetStatus() {
+		message.setText("Buffon's Needle");
+		dropsTotal.setText("0");
+		dropsCrossing.setText("0");
+		dropsMax.setText("0");
+		needleSize.setText("0");
+		precision.setText("0");
+		estimation.setText("0");
+	}
+
+	public void setDropsTotal(int x) {
+		dropsTotal.setText(Integer.toString(x));
+	}
+	public void setDropsMax(int x) {
+		dropsMax.setText(Integer.toString(x));
+	}
+	public void setDropsCrossing(int x) {
+		dropsCrossing.setText(Integer.toString(x));
+	}
+	public void setPrecision(int x) {
+		precision.setText(Integer.toString(x));
+	}
+	public void setNeedleSize(int x) {
+		needleSize.setText(Integer.toString(x));
+	}
+	public void setEstimation(double x) {
+		estimation.setText(Double.toString(x));
+	}
+	public void setMessage(String x) {
+		message.setText(x);
+	}
+}
