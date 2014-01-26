2aabc33ada124ef9f3fa1bf6bc5fb39c3d64a789
Adam Trojnar <trojnaradam@gmail.com>
2014-01-26 09:22:44 +0100

    application in almost working but have few things to improve

--- jBuff/src/jbuff/Needle.java
+++ jBuff/src/jbuff/Needle.java
@@ -1,35 +1,28 @@
 package jbuff;
 
+import java.awt.Color;
+import static java.awt.Color.red;
 import java.util.Random;
 
 public class Needle {
-	private Random generator;
-	public int hits;
-	private int tries;
-	public double ylow;
-	public double yhigh;
-	public double angle;
-	public String status;
+	 private Random generator;
+       
+        public double ylow;
+        public double yhigh;
+        public double angle;
+        public String status;
 
-	public Needle() {
-		this.hits = 0;
-		this.tries = 0;
-		generator = new Random();
-	}
-	
-	public void drop() {
-		this.ylow = 2 * generator.nextDouble();
-		this.angle = 180 * generator.nextDouble();
-		
-		this.yhigh = ylow + Math.sin(Math.toRadians(angle));
-		tries++;
-	}
-	
-	public int getHits() {
-		return this.hits;
-	}
-	
-	public int getTries() {
-		return this.tries;
-	}
+        public Needle() {
+                generator = new Random();
+        }
+        
+        public void drop() {
+                this.ylow = 6 * generator.nextDouble();
+                this.angle = 180 * generator.nextDouble();
+                
+                this.yhigh = ylow + Math.sin(Math.toRadians(angle));
+                
+        }
+        
+       
 }
